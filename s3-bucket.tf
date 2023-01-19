b9669a40a386fde7c75cf67df57af4ccbd8a30c3
saiprasanthrajavarapu <saiprasanth.rajavarapu@yahoo.in>
2023-01-19 17:57:13 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,8 @@
 module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
+  source  = "app.terraform.io/Pearson-rsp/s3-bucket/aws"
+  version = "2.8.0"
 
-  bucket = "${var.prefix}-my-s3-bucket"
+  bucket = "my-s3-bucket"
   acl    = "private"
 
   versioning = {
